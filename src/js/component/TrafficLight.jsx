5b598f036a7316b748c7ee47ebded9240e7e5b38
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-02-15 18:01:29 +0000

    Auto and Manual moode added

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import {useState} from "react";
+import React, {useState, useEffect} from "react";
 // include your styles into the webpack bundle
 
 export default function TrafficLight(){
@@ -20,11 +19,38 @@ export default function TrafficLight(){
         display: "flex",
         flexDirection: "column",
         justifyContent:"space-between",
-        padding: "10px"
+        padding: "10px",
+        marginBottom: "20px"
 
     }
     //State that will control all 3 colors 
     const [glow, setGlow] = useState("")
+    const [auto, setAuto] = useState(false)
+    
+    useEffect(() => {
+        if(auto){
+            if(glow === "red"){
+                setTimeout(()=>{
+                    setGlow("green")
+                },1000)
+            }
+            else if(glow === "green"){
+                setTimeout(()=>{
+                    setGlow("orange")
+                },1000)
+            }
+            else if(glow === "orange"){
+                setTimeout(()=>{
+                    setGlow("red")
+                },1000)
+            }
+            else {
+                setTimeout(()=>{
+                    setGlow("green")
+                },1000)
+            }
+        } 
+    },[glow,auto])
 
     //Handler function receives the color and sets the glow to that color
     function glowHandler(color){
@@ -42,10 +68,14 @@ export default function TrafficLight(){
         <div>
             <div style={trafficTopStyles}></div>
             <div style={trafficLightContainerStyle}>
-                <div className={`light light-stop ${glow === "red" ? "light-stop-on" : ""}`} onClick={() => glowHandler("red")}></div>
-                <div className={`light light-caution ${glow === "orange" ? "light-caution-on" : ""}`} onClick={() => glowHandler("orange")}></div>
-                <div className={`light light-go ${glow === "green" ? "light-go-on" : ""}`} onClick={() => glowHandler("green")}></div>
+                <div className={`light light-stop ${glow === "red" ? "light-stop-on" : ""}`} onClick={() => auto ? ()=>{} :glowHandler("red")}></div>
+                <div className={`light light-caution ${glow === "orange" ? "light-caution-on" : ""}`} onClick={() => auto ? ()=>{} :glowHandler("orange")}></div>
+                <div className={`light light-go ${glow === "green" ? "light-go-on" : ""}`} onClick={() => auto ? ()=>{} :glowHandler("green")}></div>
             </div>
+            
+             { auto === false && <button onClick={() => setAuto(true)} className="btn btn-success">GO Auto Mode</button>}
+             { auto === true && <button onClick={() => setAuto(false)} className="btn btn-success">GO Manual Mode</button>}
         </div>
+        
     )
 }
